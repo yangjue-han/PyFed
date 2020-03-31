14315c28bf1156f2d7f660e8d9f5fa183dc47761
Felix Han <yh926@nyu.edu>
2020-03-30 17:37:56 -0700

    Add Fed OMO

--- interest_rate.py
+++ interest_rate.py
@@ -26,3 +26,116 @@ class vendor:
     def __init__(self):
 
         self.fred_api = Fred(api_key='4784f4ab3b06abdc6c8cbdfa4c7825db')
+
+    def dtcc_gcf(self):
+
+        # current dataset, the data contains daily time series of the past year
+        gcf_current_url = 'https://www.dtcc.com/data/gcfindex.csv'
+        gcf_current = pd.read_csv(gcf_current_url,skipfooter=2, engine='python')
+        gcf_current.to_csv(dirpath + 'Interest rates/gcf_current.csv')
+
+        gcf_current = gcf_current.rename(
+                columns = {
+                    'MBS Total PAR Value': 'gcf_vol_mbs',
+                    'MBS Weighted Average': 'gcf_rate_mbs',
+                    'Treasury Total PAR Value': 'gcf_vol_tsy',
+                    'Treasury Weighted Average': 'gcf_rate_tsy'
+                }
+            )
+
+        gcf_current['Date'] = pd.to_datetime(gcf_current['Date'],format = '%m/%d/%Y' )
+        gcf_current.set_index('Date', inplace=True)
+
+        # historical dataset
+        gcf_hist_url = 'http://dtcc.com/data/GCF_Index_Graph.xlsx'
+        gcf_hist = pd.read_excel(gcf_hist_url, skiprows=6)
+        gcf_hist.to_csv(dirpath + 'Interest rates/gcf_hist.csv')
+
+        gcf_hist = gcf_hist[['Date', 'MBS GCF Repo® \nWeighted Average Rate',
+               'Treasury GCF Repo® \nWeighted \nAverage Rate',
+               'Agency GCF Repo® \nWeighted \nAverage Rate']].rename(
+            columns = {
+                'MBS GCF Repo® \nWeighted Average Rate':'gcf_rate_mbs',
+                'Treasury GCF Repo® \nWeighted \nAverage Rate': 'gcf_rate_tsy',
+                'Agency GCF Repo® \nWeighted \nAverage Rate': 'gcf_rate_agency'
+            }
+        ).set_index('Date')
+
+        gcf_hist.index = pd.to_datetime(gcf_hist.index,format = '%Y-%m-%d')
+
+        # so far the historical dataset extends to 2019-12-31, needs to be updated next year
+        gcf = pd.DataFrame(
+            columns=['gcf_tsy','gcf_mbs'],
+            index= pd.date_range(start="2005-01-01", end=gcf_current.index[-1])
+        )
+        gcf.loc[gcf_hist.index,'gcf_tsy'] = gcf_hist['gcf_rate_tsy'].copy()
+        gcf.loc[gcf_hist.index,'gcf_mbs'] = gcf_hist['gcf_rate_mbs'].copy()
+        gcf.loc['2020-01-01':,'gcf_tsy'] = gcf_current['gcf_rate_tsy']['2020-01-01':]
+        gcf.loc['2020-01-01':,'gcf_mbs'] = gcf_current['gcf_rate_mbs']['2020-01-01':]
+        gcf=gcf[gcf.index.dayofweek<5] # keep only business days
+
+        return gcf
+
+    def fed_repo(self, output = 'both'):
+
+        '''
+        Repo and reverse repo operations conducted by Fed, daily frequency.
+
+        The data is identified by Op ID, with each Op ID assigned to an operation
+            - Each operation has a single auction methods (e.g. single price, fixed-rate,
+                or multiple price), a single term, but different types of collateral.
+            - Principal amounts and rates are recorded at the operation-collateral level
+
+        Auction method:
+            - If the auction method is fixed-rate or single, then the rrp rate is in 'Tsy-Award'
+            - If the auction method is multiple price, then the rrp rate is in 'Tsy-Wght Avg'
+                if we are interested in the weighted average, or 'Stop-Out' if we are interested
+                in the highest bid (the stop out rate is the highest rate accepted in a reverse repo).
+        '''
+
+        fedrrp_url='https://websvcgatewayx2.frbny.org/autorates_tomo_external/services/v1_0/tomo/retrieveHistoricalExcel?f=07072000&t=03302020&ctt=true&&cta=true&ctm=true'
+        fed_op = pd.read_excel(fedrrp_url)
+        fed_op['Deal Date']=pd.to_datetime(fed_op['Deal Date'], format = '%m/%d/%Y')
+        fed_op.columns
+
+        stubs = ['Submit','Accept','Stop-Out','Award','Wght Avg','High','Low','PctAtStopOut']
+        collateral = ['Tsy','Agy','MBS']
+        suffix_mover = {}
+        for x in collateral:
+            for y in stubs:
+                suffix_mover[x+'-'+y] = y+'-'+x
+        fed_op = fed_op.rename(columns = suffix_mover)
+        identifier = ['Op ID', 'Deal Date', 'Delivery Date', 'Maturity Date', 'Op Type',
+               'Auction Method', 'Settlement', 'Term-BD', 'Term-CD', 'Op Close',
+               'Participating Counterparties', 'Accepted Counterparties',
+               'Total-Submit', 'Total-Accept']
+        fed_op = pd.wide_to_long(fed_op, stubnames = stubs, i = identifier, j = 'collateral', sep = '-', suffix='\w+')
+        fed_op.reset_index(inplace=True)
+
+        # define effective rate
+        single_rate = (fed_op['Auction Method']=='Fixed-Rate') | (fed_op['Auction Method']=='Single Price')
+        multiple_rate = (fed_op['Auction Method']=='Multiple Price')
+        fed_op.loc[single_rate,'Effective Rate'] = fed_op[single_rate]['Award']
+        fed_op.loc[multiple_rate,'Effective Rate'] = fed_op[multiple_rate]['Wght Avg']
+
+        # Daily volume by collateral
+        fed_op['acc_rate'] = fed_op['Accept'].divide(fed_op['Submit'])
+
+        # Operations volume
+        fed_op_vol = fed_op.groupby(
+            ['Deal Date','collateral','Op Type']
+        ).sum()[['Submit','Accept']].unstack().unstack()['Accept']
+        fed_op_vol = fed_op_vol[['RP','RRP']]
+
+        # Operations rate
+        fed_op_rate = fed_op.groupby(
+            ['Deal Date','collateral','Op Type']
+        ).mean()[['Effective Rate']].unstack().unstack()['Effective Rate']
+        fed_op_rate = fed_op_rate[['RP','RRP']]
+
+        if output == 'both':
+            return fed_op_rate, fed_op_vol
+        elif output == 'rate':
+            return fed_op_rate
+        elif output == 'vol':
+            return fed_op_vol
--- test.py
+++ test.py
@@ -1,8 +1,9 @@
 # Test money_stock package
-import os
-os.chdir('/Users/yangjuehan/Documents/GitHub/fed_api')
+
+#import os
+#os.chdir('/Users/yangjuehan/Documents/GitHub/fed_api')
+
 import money_stock as ms
-import FRB_H8
 import plotly.express as px
 
 # load the data vendor
@@ -14,14 +15,25 @@ H6 = get_data.FRED_H6()
 fig = px.line(H6.reset_index(), x= 'index', y = ['mmf_retail','mmf_inst'])
 fig.show()
 
+
+# Load H4 table
 H4_avg = get_data.FRED_H4_weeklyavg()
-H4_avg.plot(figsize=(15,10))
 
+H4_avg.info()
+
+fig = px.line(H4_avg.reset_index(), x='index', y='res_bal')
+fig.show()
+
+# Load H4 table
 H4_wed = get_data.FRED_H4_asofwed()
-H8 = get_data.H8()
 
-h8_api = FRB_H8.H8()
-h8_api.info()
+fig = px.line(H4_wed.reset_index(), x='index', y='res_bal')
+fig.show()
+
+
+# Load H8 data
+H8 = get_data.H8()
+H8.columns
 
 items = ['Cash assets',
          'Treasuries',
